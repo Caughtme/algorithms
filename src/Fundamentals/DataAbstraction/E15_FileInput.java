128b605282d04ca334f6074d34581a04329da8f8
yinKy <yinkaiyu553@163.com>
2022-11-14 22:17:18 +0800

    yinky

---
+++ src/Fundamentals/DataAbstraction/E15_FileInput.java
@@ -0,0 +1,16 @@
+package Fundamentals.DataAbstraction;
+
+import edu.princeton.cs.algs4.In;
+
+public class E15_FileInput {
+    public static int[] readInts(String name) {
+        In in = new In(name);
+        String input = in.readAll();
+        String[] words = input.split("\\s+");
+        int[] ints = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            ints[i] = Integer.parseInt(words[i]);
+        }
+        return ints;
+    }
+}
